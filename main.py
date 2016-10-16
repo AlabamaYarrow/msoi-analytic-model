253e07376efce315a53daef2d077976da7f6f0f7
ivan <johnmoony@mail.ru>
2016-10-17 00:38:36 +0300

    Refactor a bit

--- main.py
+++ main.py
@@ -9,48 +9,27 @@ from ui_dialog import Ui_MainWindow
 
 class UserInput:
     def __init__(self, n, to, tp, tk1, tk2, c, tpr, m, tdi, p, k1, k2, delta):
-        self.n = n
-        self.tp = tp
-        self.to = to
-        self.tk1 = tk1
-        self.tk2 = tk2
-        self.c = c
-        self.tpr = tpr
-        self.m = m
-        self.tdi = tdi
-        self.p = p
-        self.k1 = k1
-        self.k2 = k2
-        self.delta = delta
-        self.tk = 0
-        self.pi = 0
-
-    def is_valid(self):
-        try:
-            self.n = int(self.n)
-            self.tp = float(self.tp)
-            self.to = float(self.to)
-            self.tk1 = float(self.tk1)
-            self.tk2 = float(self.tk2)
-            self.c = int(self.c)
-            self.tpr = float(self.tpr)
-            self.m = int(self.m)
-            self.tdi = float(self.tdi)
-            self.p = float(self.p)
-            self.k1 = float(self.k1)
-            self.k2 = float(self.k2)
-            self.delta = float(self.delta)
-            self.tk = (self.tk1 + self.tk2) / 2
-            self.pi = 1 / self.m
-            if any([self.n < 0, self.tp < 0, self.to < 0, self.tk1 < 0,
-                    self.tk2 < 0, self.c < 0, self.tpr < 0, self.m < 0,
-                    self.tdi < 0, self.p < 0, self.p > 1, self.k2 < 10, self.k2 > 100000,
-                    self.k1 > 0.999995, self.k1 < 0.9, self.delta < 0.000001,
-                    self.delta > 0.9]):
-                raise ValueError
-        except ValueError:
-            return False
-        return True
+        self.n = int(n)
+        self.tp = float(tp)
+        self.to = float(to)
+        self.tk1 = float(tk1)
+        self.tk2 = float(tk2)
+        self.c = int(c)
+        self.tpr = float(tpr)
+        self.m = int(m)
+        self.tdi = float(tdi)
+        self.p = float(p)
+        self.k1 = float(k1)
+        self.k2 = float(k2)
+        self.delta = float(delta)
+        self.tk = (self.tk1 + self.tk2) / 2
+        self.pi = 1 / self.m
+        if any([self.n < 0, self.tp < 0, self.to < 0, self.tk1 < 0,
+                self.tk2 < 0, self.c < 0, self.tpr < 0, self.m < 0,
+                self.tdi < 0, self.p < 0, self.p > 1, self.k2 < 10, self.k2 > 100000,
+                self.k1 > 0.999995, self.k1 < 0.9, self.delta < 0.000001,
+                self.delta > 0.9]):
+            raise ValueError
 
 
 class MainWindow(QDialog, Ui_MainWindow):
@@ -60,7 +39,7 @@ class MainWindow(QDialog, Ui_MainWindow):
         self.output = {}
         self.setupUi(self)
         self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
-        self.calc_pushButton.clicked.connect(self.start_calculation)
+        self.calc_pushButton.clicked.connect(self.on_start_calculation)
 
     def show_error(self, *args):
         msg = QMessageBox()
@@ -71,55 +50,70 @@ class MainWindow(QDialog, Ui_MainWindow):
         msg.setStandardButtons(QMessageBox.Ok)
         msg.exec_()
 
-    def start_calculation(self):
-        self.input = UserInput(
-            self.n_lineEdit.text(), self.to_lineEdit.text(),
-            self.tp_lineEdit.text(), self.tk1_lineEdit.text(),
-            self.tk2_lineEdit.text(), self.c_lineEdit.text(),
-            self.tpr_lineEdit.text(), self.m_lineEdit.text(),
-            self.tdi_lineEdit.text(), self.p_lineEdit.text(),
-            self.k1_lineEdit.text(), self.k2_lineEdit.text(),
-            self.delta_lineEdit.text()
-        )
-        if not self.input.is_valid():
+    def on_start_calculation(self):
+        try:
+            self.input = UserInput(
+                self.n_lineEdit.text(), self.to_lineEdit.text(),
+                self.tp_lineEdit.text(), self.tk1_lineEdit.text(),
+                self.tk2_lineEdit.text(), self.c_lineEdit.text(),
+                self.tpr_lineEdit.text(), self.m_lineEdit.text(),
+                self.tdi_lineEdit.text(), self.p_lineEdit.text(),
+                self.k1_lineEdit.text(), self.k2_lineEdit.text(),
+                self.delta_lineEdit.text()
+            )
+        except ValueError:
             self.show_error()
-        else:
-            self.calculate()
+            self.input = None
+        self.calculate()
         self.input = None
 
     def calculate(self):
         self.output = {}
-
         self.set_output_values()
+        self.fill_table()
+        self.tabWidget.setCurrentIndex(2)
 
+    def fill_table(self):
         table_column = [self.output['ro_PC'], self.output['ro_user'], self.output['busy_PC_avg'],
                         self.output['ro_channel'], self.output['ro_pr'], self.output['ro_di'],
                         self.output['t_cycle'], self.output['t_react'], self.output['lambda_f1'],
                         self.output['lambda_f_final'], self.output['iterations']]
         for i in range(0, self.tableWidget.rowCount()):
-            self.tableWidget.setItem(i, 0,  QTableWidgetItem(str(round(table_column[i], 5))))
-
-        self.tabWidget.setCurrentIndex(2)
+            self.tableWidget.setItem(i, 0, QTableWidgetItem(str(round(table_column[i], 5))))
 
     def set_output_values(self):
         beta = 1 / (1 - self.input.p)
         self.output['beta'] = beta
 
+        self.set_labmda(beta)
+
+        lambda_ = self.input.n / self.output['t_cycle']
+        # загрузка процессора
+        self.output['ro_pr'] = beta * lambda_ * self.input.tpr / self.input.c
+        # загрузка рабочей станции
+        self.output['ro_PC'] = (self.input.to + self.input.tp) / self.output['t_cycle']
+        # среднее число работающих машин
+        self.output['busy_PC_avg'] = self.output['ro_PC'] * self.input.n
+        # загрузка пользователя
+        self.output['ro_user'] = self.input.tp / self.output['t_cycle']
+        # загрузка канала
+        self.output['ro_channel'] = 2 * lambda_ * self.input.tk
+        # загрузка i-го диска
+        self.output['ro_di'] = beta * lambda_ * self.input.tdi * self.input.pi
+
+    def set_labmda(self, beta):
         lambda_args = [
             1 / (2 * self.input.tk),
             self.input.c / (beta * self.input.tpr),
             1 / (beta * self.input.pi * self.input.tdi)
         ]
-
-        # интесивность входного потока
+        # начальная интенсивность входного потока
         lambda_f1 = self.input.k1 * min(lambda_args) * (self.input.n - 1) / self.input.n
         self.output['lambda_f1'] = lambda_f1
-
         tk_avg = self.get_tk_avg(lambda_f1)
         tpr_avg = self.get_tpr_avg(beta, lambda_f1)
         td_avg = self.get_td_avg(beta, lambda_f1)
         lambda_f = (self.input.n - 1) / (self.input.to + self.input.tp + tk_avg + tpr_avg + td_avg)
-
         iterations = 0
         while (abs(lambda_f1 - lambda_f) / lambda_f) >= self.input.delta:
             delta1 = (lambda_f1 - lambda_f) / self.input.k2
@@ -129,27 +123,14 @@ class MainWindow(QDialog, Ui_MainWindow):
             td_avg = self.get_td_avg(beta, lambda_f1)
             lambda_f = (self.input.n - 1) / (self.input.to + self.input.tp + tk_avg + tpr_avg + td_avg)
             iterations += 1
-
-        self.output['lambda_f_final'] = lambda_f
-        t_cycle = self.input.to + self.input.tp + tk_avg + tpr_avg + td_avg
-        self.output['t_cycle'] = t_cycle
-        self.output['t_react'] = tk_avg + tpr_avg + td_avg
-
+        # число итераций
         self.output['iterations'] = iterations
-
-        lambda_ = self.input.n / t_cycle
-        # загрузка процессора
-        self.output['ro_pr'] = beta * lambda_ * self.input.tpr / self.input.c
-        # загрузка рабочей станции
-        self.output['ro_PC'] = (self.input.to + self.input.tp) / t_cycle
-        # среднее число работающих машин
-        self.output['busy_PC_avg'] = self.output['ro_PC'] * self.input.n
-        # загрузка пользователя
-        self.output['ro_user'] = self.input.tp / t_cycle
-        # загрузка канала
-        self.output['ro_channel'] = 2 * lambda_ * self.input.tk
-        # загрузка i-го диска
-        self.output['ro_di'] = beta * lambda_ * self.input.tdi * self.input.pi
+        # конечное значение интенсивности
+        self.output['lambda_f_final'] = lambda_f
+        # время цикла системы
+        self.output['t_cycle'] = self.input.to + self.input.tp + tk_avg + tpr_avg + td_avg
+        # время реакции системы
+        self.output['t_react'] = tk_avg + tpr_avg + td_avg
 
     def get_tk_avg(self, lambda_f1):
         return 2 * self.input.tk / (1 - 2 * lambda_f1 * self.input.tk)
